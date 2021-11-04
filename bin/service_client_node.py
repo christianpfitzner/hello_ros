9b8b4b0aa6eb60457d768d20bc2bfd5fcf54d21a
Christian Pfitzner <christian.pfitzner@evocortex.com>
2021-11-04 21:49:06 +0100

    Added missing content of file.

---
+++ bin/service_client_node.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+
+
+
+from std_srvs.srv import *
+import rospy
+
+def setBoolCallback(req):
+    # print "Returning [%s + %s = %s]"%(req.a, req.b, (req.a + req.b))
+
+
+    return SetBoolResponse(True, "hello")
+
+
+def service_server():
+
+    rospy.init_node('service_server_python')
+
+
+    s = rospy.Service('hello_ros/set_bool_srv_py', SetBool, setBoolCallback)
+
+
+    print "Python service server is ready."
+
+    rospy.spin()
+
+
+
+if __name__ == "__main__":
+    service_server()
